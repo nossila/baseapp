ed3db89979a4d86cde1eeb0a2836431cd283d3a3
Alisson Patricio <eu@alisson.net>
2022-10-01 01:52:31 -0300

    add relay NodeField

--- backend/backend/graphql_schema.py
+++ backend/backend/graphql_schema.py
@@ -1,10 +1,38 @@
-from graphene import ObjectType, Schema
+from graphene import ObjectType, Schema, Field, ID, relay
+from graphene.relay.node import NodeField as RelayNodeField
 from pages.graphql_schema import Query as PagesQuery
 from pages.graphql_mutations import Mutation as PagesMutation
 
 
+def get_default_viewer(*args, **kwargs):
+    return Query(id='viewer')
+
+
+class NodeField(RelayNodeField):
+    def get_resolver(self, parent_resolver):
+        resolver = super().get_resolver(parent_resolver)
+
+        def get_node(instance, info, **kwargs):
+            global_id = kwargs.get('id')
+            if global_id == 'viewer':
+                return get_default_viewer(instance, info, **kwargs)
+            return resolver(instance, info, **kwargs)
+
+        return get_node
+
+
 class Query(PagesQuery, ObjectType):
-    pass
+    id = ID(required=True)
+    viewer = Field(lambda: Query)
+
+    node = NodeField(relay.Node)
+
+    class Meta:
+        interfaces = (relay.Node,)
+
+    def resolve_viewer(self, *args, **kwargs):
+        return get_default_viewer(*args, **kwargs)
+
 
 class Mutation(PagesMutation, ObjectType):
     pass
