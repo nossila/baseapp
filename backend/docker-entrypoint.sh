33e671513413df0e1605eb9506f2df90a80d4c1a
Alisson Patricio <eu@alisson.net>
2022-10-07 23:27:58 -0300

    add docker-compose and migrations

---
+++ backend/docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -xe
+python manage.py migrate # Apply database migrations
+# python manage.py collectstatic --noinput # Collect static files
+python manage.py graphql_schema --out ./public/schema.json
+
+# Prepare log files and start outputting logs to stdout
+# touch /var/log/gunicorn.log
+# touch /var/log/access.log
+# tail -n 0 -f /var/log/gunicorn.log /var/log/access.log &
+
+# Start Gunicorn processes
+echo Starting Gunicorn.
+# exec gunicorn backend.wsgi:application \
+#     --name backend \
+#     --bind 0.0.0.0:8000 \
+#     --workers $GUNICORN_WORKERS \
+#     --log-level=$GUNICORN_LOG_LEVEL \
+#     --timeout $GUNICORN_TIMEOUT \
+#     --log-file=/var/log/gunicorn.log \
+#     --access-logfile=/var/log/access.log \
+#     --reload
+#     "$@"
+exec python manage.py runserver 0.0.0.0:8000
